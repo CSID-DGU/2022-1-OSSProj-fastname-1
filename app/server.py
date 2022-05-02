fffa65012626fe7f770cd1e4450604d37624efb0
Sukhyun Kwon <sukhyun205@gmail.com>
2022-05-03 02:05:30 +0900

    modify server.py for test

--- app/server.py
+++ app/server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-   return render_template('main.html')
+   return render_template('test.html') ## 220502 for test
 
 @app.route('/login', methods = ['GET','POST'])
 def login():
