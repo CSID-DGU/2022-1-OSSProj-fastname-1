e3a074affd5a66672f86aec862d7c0842dac4530
Sukhyun Kwon <sukhyun205@gmail.com>
2022-06-04 01:16:26 +0900

    semi-final test for EC2

--- public/js/script.js
+++ public/js/script.js
@@ -108,11 +108,7 @@
 	******/
 	$('#top').click(function(){
 		$('html, body').animate({scrollTop: '0px'}, 300);
-<<<<<<< HEAD
     });
 
 	// get checkbox value
 	var check =$("input[type='checkbox']:checked").val();
-=======
-    });
->>>>>>> upstream/test
